4701c1eba9c2be1e3f3a08fc14ca1c3952b9366c
MainSilent <hamidreza.neo@gmail.com>
2021-11-04 18:53:06 +0330

    listen for connections

--- src/server.rs
+++ src/server.rs
@@ -1,15 +1,38 @@
+use std::io::BufRead;
+use std::fs;
+use std::io::{Write, BufReader};
+use std::os::unix::net::UnixListener;
+
 pub struct Server {
     path: String
 }
 
 impl Server {
     pub fn new(path: String) -> Self {
+        fs::remove_file(&path).ok();
+
         Self {
             path
         }
     }
 
     pub fn run(self) {
+        let listener = UnixListener::bind(self.path).expect("Failed to bind");
 
+        for stream in listener.incoming() {
+            match stream {
+                Ok(stream) => {
+                    let mut response = String::new();
+                    let mut stream = BufReader::new(stream);
+                    stream.read_line(&mut response).unwrap();
+
+                    println!("{}", response);
+                }
+                Err(err) => {
+                    println!("Failed to connect: {}", err);                    
+                    break;
+                }
+            }
+        }
     }
 }
