a240de93d545c1c3666f327f052874410c58c629
MainSilent <hamidreza.neo@gmail.com>
2021-11-05 21:40:20 +0330

    Update add.rs

--- src/storage/add.rs
+++ src/storage/add.rs
@@ -6,18 +6,26 @@ use std::io::prelude::*;
 use crate::config::config;
 
 pub fn add(value: &str) -> Result<&[u8], std::io::Error> {
+    if value.len() != 64 {
+        return Ok("The public key must be 64 bytes long".as_bytes())
+    }
+
+    let mut last_index: u8;
     let size_limit = 50; // size in MB
     let path = config("storage");
 
-    let files = glob(&(path.to_string()+"/*.pdb")).expect("Failed to read the pdb file");
+    let files = glob(&format!("{}/*.pdb", path)).expect("Failed to read the pdb file");
     
     if files.count() == 0 {
         add_to_file(value, &format!("{}/1.pdb", path));
+        last_index = 1;
     }
     else {
-        
+        last_index = 1;
     }
 
+    fs::write(&format!("{}/last_index", path), last_index.to_string())
+        .expect("Failed to write last index file");
     Ok("1".as_bytes())
 }
 
