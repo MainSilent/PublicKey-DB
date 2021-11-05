57136555a37eb88039542b91f709810c288a51ab
MainSilent <hamidreza.neo@gmail.com>
2021-11-05 20:44:18 +0330

    add hex

--- src/storage/add.rs
+++ src/storage/add.rs
@@ -1,5 +1,5 @@
+use hex;
 use std::fs;
-use std::u64;
 use glob::glob;
 use fs::OpenOptions;
 use std::io::prelude::*;
@@ -22,8 +22,8 @@ pub fn add(value: &str) -> Result<&[u8], std::io::Error> {
 }
 
 fn add_to_file(value: &str, path: &str) {
-    let number = u128::from_str_radix(value, 16).unwrap();
-    
+    let number = hex::decode(value).unwrap();
+
     let mut file = OpenOptions::new()
         .write(true)
         .create(true)
@@ -31,5 +31,5 @@ fn add_to_file(value: &str, path: &str) {
         .open(path)
         .expect("Failed to open database file");
 
-    file.write_all(&number.to_be_bytes()).expect("Failed to append to database");
+    file.write_all(&number).expect("Failed to append to database");
 }
