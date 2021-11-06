275668f9b29e98c610261ef3b350901bdbc812b1
MainSilent <hamidreza.neo@gmail.com>
2021-11-06 22:19:20 +0330

    Update find.rs

--- src/storage/find.rs
+++ src/storage/find.rs
@@ -1,3 +1,14 @@
+use std::fs;
+
+use crate::config::config;
+
 pub fn find(value: &str) -> &[u8] {
-    value.as_bytes()
+    if value.len() != 64 {
+        return "The public key must be 64 bytes long".as_bytes()
+    }
+    let path = config("storage");
+    let last_index_file = format!("{}/last_index", path);
+    let last_index = fs::read_to_string(&last_index_file).unwrap().parse::<u8>().unwrap();
+
+    "1".as_bytes()
 }
