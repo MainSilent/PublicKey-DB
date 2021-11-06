7ce78639fe0b09dcaadbe846f01f4f7b7ffa4774
MainSilent <hamidreza.neo@gmail.com>
2021-11-06 11:37:57 +0330

    Update add.rs

--- src/storage/add.rs
+++ src/storage/add.rs
@@ -18,7 +18,7 @@ pub fn add(value: &str) -> Result<&[u8], std::io::Error> {
     let files = glob(&format!("{}/*.pdb", path)).expect("Failed to read the pdb file");
     
     if files.count() == 0 {
-        add_to_file(value, &format!("{}/1.pdb", path));
+        sort_keys(value, &format!("{}/1.pdb", path));
         last_index = 1;
     }
     else {
@@ -31,7 +31,7 @@ pub fn add(value: &str) -> Result<&[u8], std::io::Error> {
             last_index += 1;
         }
 
-        add_to_file(value, &format!("{}/{}.pdb", path, last_index));
+        sort_keys(value, &format!("{}/{}.pdb", path, last_index));
     }
 
     fs::write(&last_index_file, last_index.to_string())
@@ -39,25 +39,24 @@ pub fn add(value: &str) -> Result<&[u8], std::io::Error> {
     Ok("1".as_bytes())
 }
 
-fn add_to_file(value: &str, path: &str) {
-    let raw_keys: &[u8];
-    let new_key = hex::decode(value).unwrap();
+fn sort_keys(value: &str, path: &str) {
+    let mut new_keys = hex::decode(value).unwrap();
 
     // Sort the data
     if fs::metadata(path).is_ok() {
         let file = fs::read(path).unwrap();
         let mut keys: Vec<&[u8]> = file.chunks(32).collect();
-        keys.push(&new_key);
+        keys.push(&new_keys);
         keys.sort();
         
-        for key in keys {
-            for byte in key {
-               raw_keys =  byte;
-            }
-        }
+        println!("{:?}", keys);
+        new_keys = [8];
+    } else {
+        add_to_file(&new_keys, path);
     }
-    
-    // Add to file
+}
+
+fn add_to_file(new_keys: &[u8], path: &str) {
     let mut file = OpenOptions::new()
         .write(true)
         .create(true)
@@ -65,5 +64,5 @@ fn add_to_file(value: &str, path: &str) {
         .open(path)
         .expect("Failed to open database file");
 
-    file.write_all(raw_keys).expect("Failed to append to database");
+    file.write_all(&new_keys).expect("Failed to append to database");
 }
