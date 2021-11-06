df7035a299ff0a4641fef4e00d2f419f3f823f3f
MainSilent <hamidreza.neo@gmail.com>
2021-11-06 20:27:46 +0330

    remove sorting from add command

--- src/storage/add.rs
+++ src/storage/add.rs
@@ -18,7 +18,7 @@ pub fn add(value: &str) -> &[u8] {
     let files = glob(&format!("{}/*.pdb", path)).expect("Failed to read the pdb file");
     
     if files.count() == 0 {
-        sort_keys(value, &format!("{}/1.pdb", path));
+        add_to_file(value, &format!("{}/1.pdb", path));
         last_index = 1;
     }
     else {
@@ -31,7 +31,7 @@ pub fn add(value: &str) -> &[u8] {
             last_index += 1;
         }
 
-        sort_keys(value, &format!("{}/{}.pdb", path, last_index));
+        add_to_file(value, &format!("{}/{}.pdb", path, last_index));
     }
 
     fs::write(&last_index_file, last_index.to_string())
@@ -39,33 +39,15 @@ pub fn add(value: &str) -> &[u8] {
     "1".as_bytes()
 }
 
-fn sort_keys(value: &str, path: &str) {
+fn add_to_file(value: &str, path: &str) {
     let new_key = hex::decode(value).unwrap();
 
-    // Sort the data
-    if fs::metadata(path).is_ok() {
-        let file = fs::read(path).unwrap();
-        let mut keys: Vec<&[u8]> = file.chunks(32).collect();
-        keys.push(&new_key);
-        keys.sort();
-        
-        let new_keys = keys.iter()
-            .flat_map(|arr| arr.iter())
-            .cloned()
-            .collect::<Vec<u8>>();
-
-        add_to_file(&new_keys, path);
-    } else {
-        add_to_file(&new_key, path);
-    }
-}
-
-fn add_to_file(new_keys: &[u8], path: &str) {
     let mut file = OpenOptions::new()
         .write(true)
         .create(true)
+        .append(true)
         .open(path)
         .expect("Failed to open database file");
 
-    file.write_all(&new_keys).expect("Failed to append to database");
+    file.write_all(&new_key).expect("Failed to append to database");
 }
--- src/storage/sort.rs
+++ src/storage/sort.rs
@@ -1,3 +1,23 @@
 pub fn sort() -> &'static [u8] {
     "value".as_bytes()
 }
+
+// fn sort_keys(value: &str, path: &str) {
+
+//     // Sort the data
+//     if fs::metadata(path).is_ok() {
+//         let file = fs::read(path).unwrap();
+//         let mut keys: Vec<&[u8]> = file.chunks(32).collect();
+//         keys.push(&new_key);
+//         keys.sort();
+        
+//         let new_keys = keys.iter()
+//             .flat_map(|arr| arr.iter())
+//             .cloned()
+//             .collect::<Vec<u8>>();
+
+//         add_to_file(&new_keys, path);
+//     } else {
+//         add_to_file(&new_key, path);
+//     }
+// }
